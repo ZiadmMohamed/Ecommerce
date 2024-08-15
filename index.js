5267b20d1ce2989bb53ecd12ede8a6650c5a00e8
Ziad Yassin <ziady5060@gmail.com>
2024-08-15 21:18:48 +0300

    .

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(express.json());
+app.use((req, res, next) => {
+  if (req.originalUrl == "/order/webhook") {
+    return next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
 
 app.get("/", (req, res) => res.send("Hello to my Ecommerce project"));
 
